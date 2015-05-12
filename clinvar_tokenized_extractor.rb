83ccbba8557706ac484b021d2b445476e3448d9e
Xin Feng <xinf@bcm.edu>
2015-05-12 13:47:08 -0500

    init

---
+++ clinvar_tokenized_extractor.rb
@@ -0,0 +1,84 @@
+# Get an element of clinvar xml
+#
+# @Author Xin Feng
+# @Date 05/12/2015
+#
+#
+#
+require 'progressPrinter'
+require 'logging'
+
+class ClinVarXMLTokenizedExtractor
+  def initialize(file, id)
+    @file = File.open(file,'rb')
+    @log = Logging.logger(STDERR)
+    @log.level = :debug
+    @clinvar_set_locs = []
+    @start_line_no =  []
+    @end_line_no =  []
+    @total_lines_cnt = 0
+    @clinvar_set_ids = []
+    @KEY1 = '<ClinVarSet'
+    @KEY2 = '/ClinVarSet'
+    @id = id
+    @lines_buffer = ''
+  end
+
+  def get_clinvar_set_locs_and_ids
+    @file.each_line do |line|
+      @total_lines_cnt += 1
+      if line.include? @KEY1
+        @start_line_no << @file.lineno
+        @clinvar_set_ids << line.match(/<ClinVarSet ID=(.*)>/)[0]
+      elsif line.include? @KEY2
+        @end_line_no << @file.lineno
+      end
+    end
+    if @start_line_no.length != @end_line_no.length
+      raise "Incomplete clinvarset detected."
+    end
+    @file.rewind
+  end
+
+  def parse str
+    puts @lines_buffer
+  end
+
+  def find_ind 
+    @clinvar_set_ids.each_with_index do |id,ind|
+      if id.include? @id
+        return ind
+      end
+    end
+    return nil
+  end
+
+  def run
+    @log.info "Finding locations of each set"
+    get_clinvar_set_locs_and_ids
+    @log.info "..Done"
+    @log.info "Total lines in the xml file:#{@total_lines_cnt}"
+    @log.info "Total records in the xml file:#{@start_line_no.length}"
+    dp = ProgressPrinter.new(@start_line_no.length)
+
+    ind = find_ind 
+    raise "Didnt find your id" if ind.nil?
+    line_loc_ind = 0
+    line_cnt = 1
+    start_l_loc =  @start_line_no[ind]
+    end_l_loc =  @end_line_no[ind]
+
+    @file.each_line do |line|
+      if line_cnt >= start_l_loc && line_cnt <= end_l_loc 
+        @lines_buffer += line
+      elsif line_cnt > end_l_loc
+        parse @lines_buffer
+        return
+      end
+      line_cnt += 1
+    end
+    
+    parse @lines_buffer
+
+  end
+end
